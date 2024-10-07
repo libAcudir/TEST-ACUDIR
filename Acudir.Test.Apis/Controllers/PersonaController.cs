2a9daed10767e142f7ddef07eab00d877ff0e97e
Unnamed18 <49623130+Unnamed18@users.noreply.github.com>
2024-10-07 11:15:15 -0300

    Add files via upload

---
+++ Acudir.Test.Apis/Controllers/PersonaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Acudir.Test.Apis.Models;
+using NombreDelProyecto.Interfaces;
+
+namespace Acudir.Test.Apis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonaController : ControllerBase
+    {
+        private readonly IPersonaRepository _personaRepository;
+
+        public PersonaController(IPersonaRepository personaRepository)
+        {
+            _personaRepository = personaRepository;
+        }
+
+        // Endpoint GET para obtener todas las personas, con filtros opcionales
+        [HttpGet("GetAll")]
+        public IActionResult GetAll([FromQuery] string nombre, [FromQuery] int? edad)
+        {
+            var personas = _personaRepository.GetAll(nombre, edad);
+            return Ok(personas);
+        }
+
+        // Endpoint POST para agregar una nueva persona
+        [HttpPost]
+        public IActionResult Add([FromBody] Persona nuevaPersona)
+        {
+            _personaRepository.Add(nuevaPersona);
+            return Ok();
+        }
+
+        // Endpoint PUT para modificar una persona existente
+        [HttpPut]
+        public IActionResult Update([FromBody] Persona personaModificada)
+        {
+            _personaRepository.Update(personaModificada);
+            return Ok();
+        }
+    }
+}
